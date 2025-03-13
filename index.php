ad8f4846cf140f9f53530aa9288f77d2ec3e46ab
Sofia <aconnor0213+sofia@gmail.com>
2025-03-13 13:12:36 +0000

    copied this code for ip address

--- index.php
+++ index.php
@@ -7,9 +7,19 @@
 </head>
 <body>
     <h1>sofiaslost.co.uk Hotspot</h1>
-    <?php
-    $interface_to_detect = 'swlan0';
-    echo explode(' ',explode(':',explode('inet addr',explode($interface_to_detect,trim(`ifconfig`))[1])[1])[1])[0];
+    <?php   
+        // if user from the share internet   
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {   
+            echo 'IP address = '.$_SERVER['HTTP_CLIENT_IP'];   
+        }   
+        //if user is from the proxy   
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   
+            echo 'IP address = '.$_SERVER['HTTP_X_FORWARDED_FOR'];   
+        }   
+        //if user is from the remote address   
+        else{   
+            echo 'IP address = '.$_SERVER['REMOTE_ADDR'];   
+        }     
     ?>
 </body>
 </html>
