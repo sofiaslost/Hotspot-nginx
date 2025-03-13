3c1e864e854c0cc6b0c06fc052d7f35c07b40dd1
Sofia <aconnor0213+sofia@gmail.com>
2025-03-13 12:55:25 +0000

    Remake main html file into php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nginx Admin Panel</title>
+</head>
+<body>
+    <h1>sofiaslost.co.uk Hotspot</h1>
+    <?php
+    $ip = gethostbyname("10.150.74.24");
+    echo $ip;
+    ?>
+</body>
+</html>
