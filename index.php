3d35b226ac60841070dab3702c113cf39461625d
Sofia <aconnor0213+sofia@gmail.com>
2025-03-14 10:11:07 +0000

    Added style and updated ip finding script

--- index.php
+++ index.php
@@ -3,23 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Nginx Admin Panel</title>
 </head>
 <body>
     <h1>sofiaslost.co.uk Hotspot</h1>
-    <?php   
-        // if user from the share internet   
-        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {   
-            echo 'IP address = '.$_SERVER['HTTP_CLIENT_IP'];   
-        }   
-        //if user is from the proxy   
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   
-            echo 'IP address = '.$_SERVER['HTTP_X_FORWARDED_FOR'];   
-        }   
-        //if user is from the remote address   
-        else{   
-            echo 'IP address = '.$_SERVER['REMOTE_ADDR'];   
-        }     
-    ?>
+
+    <?php getUserIP(); ?>
 </body>
 </html>
+
+<?php   
+    function getUserIP() {
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'COULD NOT FIND IP ADDRESS';
+        return $ipaddress;
+        echo $ipaddress;
+    }
+?>
